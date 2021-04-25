c324e6348c86b1528728dc18bab4c5781ea90c25
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2021-04-24 23:18:18 -0500

    adding random data to sample data

--- scripts/load_sample_data.py
+++ scripts/load_sample_data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from pymongo import MongoClient
@@ -6,9 +7,18 @@ from pymongo import MongoClient
 # Extra cleaning
 data = pd.read_csv("dentists.csv")
 data.drop('Unnamed: 0', axis=1, inplace=True)
-data["specialization"] = data["specialization"].apply(lambda x: [x])
+data["specialties"] = data["specialization"].apply(lambda x: [x])
+data.drop('specialization', axis=1, inplace=True)
 data["middle_name"].fillna("", inplace=True)
 data["address_2nd_line"].fillna("", inplace=True)
+
+# Add coluumns with random values:
+np.random.seed(42)
+data['price'] = np.random.randint(9, 106, data.shape[0])
+data['rate'] = np.random.uniform(1, 5, data.shape[0])
+data['reviews'] = [[]] * data.shape[0]
+
+# Load to Mongo
 
 records = data.to_dict("records")
 
